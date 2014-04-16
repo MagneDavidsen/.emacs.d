3941ab66e18142c80c9f86faea32a6fb2d3b0dfe
Stig Henriksen <mail@stighenriksen.com>
2014-04-16 12:05:11 +0200

    Add company-backend for haskell.

--- company-mode.el
+++ company-mode.el
@@ -1,1 +1,16 @@
+(require 'company)
+(require 'haskell-process)
 (add-hook 'after-init-hook 'global-company-mode)
+
+(defun company-haskell-process-backend (command &optional arg &rest ignored)
+  (interactive (list 'interactive))
+  (case command
+    (interactive (company-begin-backend 'company-haskell-process-backend))
+    (prefix (if (and (eq major-mode 'haskell-mode) (haskell-session-maybe)) (company-grab-word) nil))
+    (candidates 
+     (let ((resp (haskell-process-get-repl-completions (haskell-process) arg)))
+           (if (string= (car resp) "")
+               (cdr resp)
+             resp)))))
+
+(setq company-backends (cons 'company-haskell-process-backend company-backends))
